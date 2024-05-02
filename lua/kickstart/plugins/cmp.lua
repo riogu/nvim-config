c26a073936e33bfc0ae5d23b6d707ffdd89ac4e9
shnCanos <catellacatellacarettacaretta@gmail.com>
2024-05-02 20:39:19 +0100

    added tab autocompletion

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -67,6 +67,8 @@ return {
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- HEY! Tab deserves autocompletion too!
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
