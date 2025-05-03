a33a20e246d7743af1d4679834a289f416db191d
riogu <egas.g.ribeiro@gmail.com>
2025-05-03 14:25:14 +0100

    changes for theme

--- lua/options.lua
+++ lua/options.lua
@@ -48,7 +48,9 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { trail = '⚬', tab = '>-', nbsp = '␣' }
+
 -- vim.opt.wrap = false
 
 -- Preview substitutions live, as you type!
