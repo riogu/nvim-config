d0bb7eb8b65f189fc0411ac7b94b8b4c5bd7aea8
riogu <egas.g.ribeiro@gmail.com>
2025-10-05 23:10:51 +0100

    updated gdb debugging for improved ease of testing

--- lua/custom/plugins/nvim-gdb.lua
+++ lua/custom/plugins/nvim-gdb.lua
@@ -130,18 +130,141 @@ return {
       end
     end, { nargs = '+', complete = 'file' })
 
-    -- Alternative: Manual cc1plus command if you know the exact flags
-    vim.api.nvim_create_user_command('GdbCC1plusDirect', function(opts)
+    -- Quick command with custom std and base flags
+    vim.api.nvim_create_user_command('GdbCC1plusQuick', function(opts)
+      local args = vim.split(opts.args, '%s+')
+      if #args < 2 then
+        vim.notify('Usage: GdbCC1plusQuick <test_file> <std_version> [extra_flags]', vim.log.levels.ERROR)
+        return
+      end
+
+      local test_file = args[1]
+      local std_version = args[2]
+      local extra_flags = ''
+      if #args > 2 then
+        extra_flags = table.concat(vim.list_slice(args, 3), ' ')
+      end
+
       local cmd = string.format(
-        'GdbStart gdb --args ./cc1plus -quiet -std=c++20 '
+        'GdbStart gdb --args ./cc1plus -quiet -std=c++%s '
           .. '-nostdinc++ '
           .. '-isystem ../x86_64-pc-linux-gnu/libstdc++-v3/include '
           .. '-isystem ../x86_64-pc-linux-gnu/libstdc++-v3/include/x86_64-pc-linux-gnu '
           .. '-isystem ../../gcc/libstdc++-v3/libsupc++ '
-          .. '%s',
-        opts.args
+          .. '%s %s',
+        std_version,
+        extra_flags,
+        test_file
       )
       vim.cmd(cmd)
+    end, { nargs = '+', complete = 'file' })
+
+    -- Run DejaGNU test and show results
+    vim.api.nvim_create_user_command('RunTest', function(opts)
+      local test_file = opts.args
+
+      -- Parse DejaGNU options
+      local dejagnu_opts = parse_dejagnu_options(test_file)
+
+      local cmd = string.format(
+        './xg++ -B. -nostdinc++ '
+          .. '-isystem ../x86_64-pc-linux-gnu/libstdc++-v3/include '
+          .. '-isystem ../x86_64-pc-linux-gnu/libstdc++-v3/include/x86_64-pc-linux-gnu '
+          .. '-isystem ../../gcc/libstdc++-v3/libsupc++ '
+          .. '%s %s',
+        dejagnu_opts,
+        test_file
+      )
+
+      vim.notify('Running test: ' .. test_file, vim.log.levels.INFO)
+
+      -- Run in terminal
+      vim.cmd('split | terminal ' .. cmd)
     end, { nargs = 1, complete = 'file' })
+
+    -- Show DejaGNU directives from test file
+    vim.api.nvim_create_user_command('ShowTestOptions', function(opts)
+      local test_file = opts.args
+      local dejagnu_opts = parse_dejagnu_options(test_file)
+
+      if dejagnu_opts ~= '' then
+        vim.notify('DejaGNU options: ' .. dejagnu_opts, vim.log.levels.INFO)
+      else
+        vim.notify('No DejaGNU options found in ' .. test_file, vim.log.levels.WARN)
+      end
+    end, { nargs = 1, complete = 'file' })
+
+    -- Search for test files by pattern
+    vim.api.nvim_create_user_command('FindTest', function(opts)
+      local pattern = opts.args
+      local find_cmd = string.format("find ../../gcc/testsuite/g++.dg -name '*%s*.C' -o -name '*%s*.cc'", pattern, pattern)
+
+      local handle = io.popen(find_cmd)
+      local results = handle:read '*a'
+      handle:close()
+
+      local lines = {}
+      for line in results:gmatch '[^\r\n]+' do
+        table.insert(lines, line)
+      end
+
+      if #lines == 0 then
+        vim.notify('No tests found matching: ' .. pattern, vim.log.levels.WARN)
+      elseif #lines == 1 then
+        vim.notify('Found: ' .. lines[1], vim.log.levels.INFO)
+        vim.cmd('edit ' .. lines[1])
+      else
+        -- Create a scratch buffer with just file paths
+        local buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+        vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+        vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+        vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+        vim.api.nvim_buf_set_name(buf, 'Test Results: ' .. pattern)
+
+        -- Open in split
+        vim.cmd 'split'
+        vim.api.nvim_win_set_buf(0, buf)
+
+        -- Add keymaps for easy usage
+        local function get_current_path()
+          local line = vim.api.nvim_get_current_line()
+          return vim.fn.fnameescape(line)
+        end
+
+        vim.api.nvim_buf_set_keymap(buf, 'n', '<CR>', '', {
+          noremap = true,
+          silent = true,
+          desc = 'Open test file',
+          callback = function()
+            local path = get_current_path()
+            vim.cmd 'wincmd p'
+            vim.cmd('edit ' .. path)
+          end,
+        })
+        vim.api.nvim_buf_set_keymap(buf, 'n', 'd', '', {
+          noremap = true,
+          silent = true,
+          desc = 'Debug this test',
+          callback = function()
+            local path = get_current_path()
+            vim.cmd 'wincmd p'
+            vim.cmd('GdbCC1plus ' .. path)
+          end,
+        })
+        vim.api.nvim_buf_set_keymap(buf, 'n', 'r', '', {
+          noremap = true,
+          silent = true,
+          desc = 'Run test',
+          callback = function()
+            local path = get_current_path()
+            vim.cmd 'wincmd p'
+            vim.cmd('RunTest ' .. path)
+          end,
+        })
+
+        vim.notify(string.format('Found %d tests. <CR>=open, d=debug, r=run', #lines), vim.log.levels.INFO)
+      end
+    end, { nargs = 1 })
   end,
 }
